334bb7ed4c522592680f6a50fc31e7ed61697597
Izabela Poirier <izabelapoirier@gmail.com>
2020-04-23 22:48:44 +0200

    cleanup

--- dbconf.py
+++ dbconf.py
@@ -1,39 +1,14 @@
-# Python code to illustrate
-# inserting data in MongoDB
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
-def dbconnect(name):
+
+def connect_to_db():
     try:
         conn = MongoClient()
         print("Connected successfully!!!")
-    except:
+    except ConnectionFailure:
         print("Could not connect to MongoDB")
 
-    # database
-    db = conn.database
-
-    # Created or Switched to collection names: my_gfg_collection
-    collection = db.blockchain
+    db = conn.database   # database
+    collection = db.blockchain    # Created or Switched to collection
     return collection
-
-emp_rec1 = {
-    "name": "Mr.Geek",
-    "eid": 24,
-    "location": "delhi"
-}
-emp_rec2 = {
-    "name": "Mr.Shaurya",
-    "eid": 14,
-    "location": "delhi"
-}
-
-# Insert Data
-# rec_id1 = collection.insert_one(emp_rec1)
-# rec_id2 = collection.insert_one(emp_rec2)
-
-# print("Data inserted with record ids", rec_id1, " ", rec_id2)
-#
-# # Printing the data inserted
-# cursor = collection.find()
-# for record in cursor:
-#     print(record)
