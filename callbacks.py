8af3eb3ce548d8cc410838014544f4cfcb53ed27
Izabela Poirier <izabelapoirier@gmail.com>
2020-05-04 22:54:34 +0200

    Add few comments

--- callbacks.py
+++ callbacks.py
@@ -9,7 +9,7 @@ import dbconf
 import initialization
 import security
 from bson import BSON
-import helpers.utils
+import helpers.utils as utils
 import main
 import data_collector
 MAX_BLOCKS = 3
@@ -32,10 +32,6 @@ def find_device_public_key(list_devices, id_device):
     filter_obj = list(filter(lambda x: x.id == id_device, main.list_devices))
     return PublicKey(filter_obj[0].public_key_n, filter_obj[0].public_key_e)
 
-
-def delete_device_from_storages(id_device):
-    filter_obj = list(filter(lambda x: x.id != id_device, main.list_devices))
-    main.list_devices = filter_obj
 
 def find_mac_address(list_devices, id_device):
     filter_obj = list(filter(lambda x: x.id == id_device, main.list_devices))
@@ -61,13 +57,13 @@ def validate_blocks(client, validated_blocks):
         if find_mac_address(main.list_devices, i['id']) == i['mac'] and decrypted_message_verification:
             new_block.update({str(iterator): i})
             iterator += 1
-            client.publish(helpers.utils.CORRECT_VALIDATION, i['id'])
+            client.publish(utils.CORRECT_VALIDATION, i['id'])
         else:
-            client.publish(helpers.utils.FALSE_VALIDATION, i['id'])
+            client.publish(utils.FALSE_VALIDATION, i['id'])
             print("fake block")
             return
     new_block.update({"time": str(datetime.datetime.utcnow())})
-    client.publish(helpers.utils.NEW_BLOCK, BSON.encode(new_block))
+    client.publish(utils.NEW_BLOCK, BSON.encode(new_block))
 
 
 def add_new_block(client, userdata, message):
@@ -119,39 +115,74 @@ def decrement_trust_value(client, userdata, message):
 
 def choose_new_miner(client, userdata):
     #TODO check if not null!
-    could_be_miner = dict(filter(lambda elem: int(elem[0]) > 10, main.trusted_devices.items()))
-    print(could_be_miner)
-    new_miner = random.choice(list(could_be_miner.keys()))
-    client.publish(helpers.utils.CHOOSE_MINER, new_miner)
+    #TODO not a callback -> move to another file
+    try:
+        could_be_miner = dict(filter(lambda elem: int(elem[0]) > 10, main.trusted_devices.items()))
+        print(could_be_miner)
+        new_miner = random.choice(list(could_be_miner.keys()))
+        client.publish(utils.CHOOSE_MINER, new_miner_status)
+    except KeyError:
+        pass
 
 
-def new_miner(client, userdata, message):
+def new_miner_status(client, userdata, message):
+    """Callback for CHOOSE_MINER
+    message.payload - device_id of chosen new miner for next block
+    """
     if str(message.payload, "UTF-8") == userdata.get("id_device"):
         main.is_miner = True
     else:
         main.is_miner = False
 
 
-def resend_device_info(client, userdata, message):
-    tmp_object = json.loads(message.payload)
-    if list(filter(lambda x: x.id == tmp_object['id'], main.list_devices)).__len__() == 0:
-        main.list_devices.append(
-            initialization.DeviceInfo(tmp_object['id'], tmp_object['mac_address'], "client/" + str(tmp_object['id']),
-                                      tmp_object['public_key_e'], tmp_object['public_key_n']))
-    client.publish(helpers.utils.PUB_KEYS_TOPIC, json.dumps(main.list_devices[0].__dict__))
+def receive_and_send_device_info(client, userdata, message):
+    """Callback for NEW_DEVICE_DEVICE_INFO
+    Takes place when a message with device info is received from other device.
+    Adds to list of devices and sends information about own device.
+    message.payload = {id: ...,
+                        mac_address: ...,
+                        client/id_device: ...,
+                        public_key_e: ...,
+                        public_key_n: ...
+                        }
+    """
+    try:
+        received_device_info = json.loads(message.payload)
+        if list(filter(lambda x: x.id == received_device_info['id'], main.list_devices)).__len__() == 0:
+            main.list_devices.append(
+                initialization.DeviceInfo(received_device_info['id'],
+                                          received_device_info['mac_address'],
+                                          "client/" + str(received_device_info['id']),
+                                          received_device_info['public_key_e'],
+                                          received_device_info['public_key_n']))
+        client.publish(utils.NEW_DEVICE_INFO, json.dumps(main.list_devices[0].__dict__))
+    except KeyError:
+        pass
 
 
-def resend_trust_rate(client, userdata, message):
-    temp = json.loads(message.payload)
-    main.trusted_devices.update(temp)
-    client.publish(helpers.utils.TRUST_RATE,
-                   json.dumps({userdata.get("id_device"): int(main.trusted_devices.get(userdata.get("id_device")))}))
+def receive_and_send_trust_rate(client, userdata, message):
+    """Callback for NEW_DEVICE_TRUST_RATE
+    Takes place when a message with trust rate value is received from other device.
+    Adds to dictionary trust rate and sends current trust rate for device.
+    message.payload = {id_device : trust_rate}
+    """
+    received_trust_rate = json.loads(message.payload)
+    try:
+        main.trusted_devices.update(received_trust_rate)
+        client.publish(utils.RESPOND_WITH_OWN_TRUST_RATE,
+                       json.dumps({userdata.get("id_device"): int(main.trusted_devices.get(userdata.get("id_device")))}))
+    except KeyError:
+        pass
 
 
 def delete_device(client, userdata, message):
+    """Callback for Last will message - DEVICE_OFFLINE
+    Takes care of removing information about device
+    message.payload = device_id
+    """
     inactive_device_id = str(message.payload, "UTF-8")
     try:
-        delete_device_from_storages(inactive_device_id)
+        main.list_devices = list(filter(lambda x: x.id != inactive_device_id, main.list_devices))
         main.trusted_devices.pop(inactive_device_id)
     except KeyError:
         pass
