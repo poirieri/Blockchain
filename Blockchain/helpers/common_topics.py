76c0448b84b89acfbd6027ab7971306bffb9375c
Izabela Poirier <izabelapoirier@gmail.com>
2020-05-17 14:10:01 +0200

    Shorten topics

--- Blockchain/helpers/common_topics.py
+++ Blockchain/helpers/common_topics.py
@@ -3,18 +3,19 @@ SYS_BROKER_BYTES_RECEIVED = "$SYS/broker/load/bytes/received"
 SYS_BROKER_BYTES_SEND = "$SYS/broker/load/bytes/sent"
 SYS_BROKER_NUMBER_CLIENTS_CONNECTED = "$SYS/broker/clients/total"
 
-NEW_DEVICE_INFO = "client/send/deviceinfo"
-NEW_DEVICE_INFO_RESPOND = "client/send/resend/deviceinfo"
-DEVICE_OFFLINE = "client/offline"
-NEW_DEVICE_TRUST_RATE = "client/trustvalue/new"
-SEND_MESSAGE_TOPIC = "client/receive/#"
-SEND_ENCRYPTED_MESSAGE = "client/encrypted/receive/"
-MAXIMUM_BLOCK_SIZE = 35
-INCREMENT_TRUST_VALUE = "client/trustvalue/add/"
-DECREMENT_TRUST_VALUE = "client/trustvalue/decrement"
-CHOOSE_MINER = "client/trustvalue/choose"
-CHECK_BLOCK = "client/trustvalue/check"
-RESPOND_WITH_OWN_TRUST_RATE = "client/trustvalue"
-NEW_BLOCK = "client/block/new"
-FALSE_VALIDATION = "client/trustvalue/decrement"
-CORRECT_VALIDATION = "client/trustvalue/add/"
+""" TOPICS ABBREVIATIONS
+tv - trust value topics
+dev_info - device info topics
+sb - single block
+"""
+
+NEW_DEVICE_INFO = "dev_info/send"
+NEW_DEVICE_INFO_RESPOND = "dev_info/respond"
+SEND_ENCRYPTED_MESSAGE = "sb/encrypted"
+NEW_BLOCK = "block/new"
+DEVICE_OFFLINE = "offline"
+NEW_DEVICE_TRUST_RATE = "tv/new"
+CHOOSE_MINER = "tv/choose"
+RESPOND_WITH_OWN_TRUST_RATE = "tv/respond"
+FALSE_VALIDATION = "tv/decrement"
+CORRECT_VALIDATION = "tv/add"
